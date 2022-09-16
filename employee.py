7a13c090fd32e3b82ede780ef25263d6c2ce7782
LilithCole <lilith.cole@stfc.ac.uk>
2022-09-16 14:17:27 +0100

    Added docstrings

--- employee.py
+++ employee.py
@@ -10,11 +10,21 @@ class Employee:
         self.employee_id = uuid.uuid4()
 
     def update_contact_info(self, name: str) -> str:
+        """
+        Method which updates the contact info of this employee
+        :param name: New name for this employee
+        :return: str: the previous name of this employee
+        """
         past_name = self.name
         self.name = name
         return past_name
 
     def promote_employee(self, job_title):
+        """
+        Promote this employee to a new job title
+        :param job_title: The new job title for this employee
+        :return: None
+        """
         if job_title not in JOB_TITLES:
             raise ValueError(
                 f"Invalid job title {job_title}, expected one of {JOB_TITLES}"
--- office.py
+++ office.py
@@ -16,10 +16,24 @@ class TheOffice:
         return f"Welcome to The Office! The boss here is {self.regional_manager}."
 
     def record_sale(self, employee: Employee) -> None:
+        """
+        Method which records a sale made by an employee
+        :param employee: The employee which made the sale
+        :return: None
+        """
         self.sales[employee] += 1
 
     def get_employee_sales(self, employee: Employee) -> int:
+        """Method that returns the number of sales given an employee.
+        :param employee: The employee whose sales number we are interested in
+        :return: int: The total number of sales the employee as made
+        """
         return self.sales[employee]
 
     def _fire_employee(self, employee: Employee) -> None:
+        """
+        Method which removes an employee from our database
+        :param employee: The employee which we wish to remove
+        :return: None
+        """
         self.employees.remove(employee)
